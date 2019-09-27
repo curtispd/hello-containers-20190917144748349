2d896ccd052ff36ff316fe0c9f4000a6a0f44494
Curtis d'Entremont <curtispd@ca.ibm.com>
2019-09-27 14:18:34 -0400

    test

---
+++ scripts/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+###############################################################################
+# Licensed Materials - Property of IBM
+# (c) Copyright IBM Corporation 2019. All Rights Reserved.
+#
+# Note to U.S. Government Users Restricted Rights:
+# Use, duplication or disclosure restricted by GSA ADP Schedule
+# Contract with IBM Corp.
+###############################################################################
+
+set -o pipefail
+
+echo "hello, world!"
+
+exit 0
